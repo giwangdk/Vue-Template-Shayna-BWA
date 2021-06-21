d72f29d1b3a5046ae87b8c80c9d564d7798cba9c
gidwikintan <gidwikintan@gmail.com>
2021-06-22 00:54:51 +0700

    finish convert html to vue

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: Product
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: Success
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <Hero/>
+    <Banner/>
+    <InstaShayna/>
+    <PartnerLogo/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+
+
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import Hero from "@/components/Hero.vue";
+import Banner from "@/components/Banner.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    Hero,
+    Banner,
+    InstaShayna,
+    PartnerLogo,
+    Footer
+  },
+};
+</script>

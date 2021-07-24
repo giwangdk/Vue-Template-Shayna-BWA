156334caf1d9a62dcfb166ae74db067c32463bb3
gidwikintan <gidwikintan@gmail.com>
2021-07-24 16:46:25 +0700

    Integrasi API with axios

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,6 +4,8 @@
             <div class="row">
                 <div class="col-lg-12 mt-5">
                     <carousel class="product-slider" :loop="true" :dots="false" :nav="false" :items="3" :autoplay="true">
+                        
+                        
                         <div class="product-item">
                             <div class="pi-pic">
                                 <img src="img/mickey1.jpg" alt="" />
@@ -26,69 +28,6 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
                     </carousel>
                 </div>
             </div>
@@ -99,10 +38,23 @@
 <script>
 
 import carousel from "vue-owl-carousel";
+import axios from 'axios';
+
 export default {
     name:"Banner",
     components:{
         carousel,
+    },
+    data(){
+        return{
+            products:[]
+        }
+    },
+    mounted(){
+        axios
+        .get("http://127.0.0.1:8000/api/products")
+        .then(res=>(this.products =res.data))
+        .catch(err=>console.lof(err));
     }
 }
 </script>
